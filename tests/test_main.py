4e61bde12b72b91ae4b4cfc26059584c9a464c8a
Gvilor <gvilor@yandex.ru>
2026-03-25 21:28:10 +0800

    demo2

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, CI/CD Lab UPDATED"}
+    assert response.json() == {"message": "Hello, CI/CD Lab demo"}
 
 
 def test_health_check():
