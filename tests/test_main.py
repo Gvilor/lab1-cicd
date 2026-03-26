650a7dfb52feecc517a93e8710ffe99b7b962a70
georgiy_vilor <gvilor@yandex.ru>
2026-03-26 13:07:21 +0800

    test1

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, CI/CD Lab demo"}
+    assert response.json() == {"message": "Hello, from docker test1"}
 
 
 def test_health_check():
